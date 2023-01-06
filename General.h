3902024ab88745d939b28d09a73818a5b2e1eac5
Shanbo-Zhang <shanboz2@illinois.edu>
2023-01-06 17:19:23 -0600

    First

---
+++ General.h
@@ -0,0 +1,43 @@
+//
+// Created by Escap on 1/6/2023.
+//
+
+#ifndef ESCAPIST_GENERAL_H
+#define ESCAPIST_GENERAL_H
+
+// Platform detection.
+
+#if defined(WIN32) | defined(_WIN32) | defined(__WIN32__)
+#define ESCAPIST_OS_WINDOWS
+#ifdef _WIN64
+#define ESCAPIST_WIN_64BITS
+#else
+#define ESCAPIST_WIN_32BITS
+#endif
+#elif defined(__linux__)
+#define ESCAPIST_OS_LINUX
+#elif defined(__unix__)
+#define ESCAPIST_OS_UNIX
+#elif
+#error "Unsupported Platform"
+#endif
+
+// Start defining some basic types based on different platforms
+#ifdef ESCAPIST_OS_WINDOWS
+
+#include <winsock2.h>
+#include <windows.h>
+#include <cassert>
+
+using Handle = void *;
+#if defined(ESCAPIST_WIN_64BITS)
+using Char = wchar_t;
+using SizeType = unsigned long long;
+#define UNICODE
+#elif defined(ESCAPIST_WIN_32BITS)
+using Char = char;
+using SizeType = unsigned int;
+#endif
+#endif
+
+#endif //ESCAPIST_GENERAL_H
---
+++ Internal/TypeTrait.h
@@ -0,0 +1,15 @@
+//
+// Created by Escap on 1/6/2023.
+//
+
+#ifndef ESCAPIST_TYPETRAIT_H
+#define ESCAPIST_TYPETRAIT_H
+
+#include "../General.h"
+
+template<typename T>
+class TypeTrait {
+
+};
+
+#endif //ESCAPIST_TYPETRAIT_H

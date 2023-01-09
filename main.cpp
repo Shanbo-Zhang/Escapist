e74daf629a7ac6e8f625772053dcd7fba7cdaf25
Shanbo-Zhang <shanboz2@illinois.edu>
2023-01-09 02:31:58 -0600

    ArrayList.h

--- main.cpp
+++ main.cpp
@@ -20,6 +20,4 @@ int main() {
     std::wcout << str.GetConstData() << std::endl;
     std::wcout << str.GetLength() << std::endl;
     std::wcout << str.GetCapacity() << std::endl;
-    std::wcout << wcslen(str.GetConstData()) << std::endl;
-    std::wcout << String (L"12312390890890899080890123").Reverse().GetConstData();
 }
